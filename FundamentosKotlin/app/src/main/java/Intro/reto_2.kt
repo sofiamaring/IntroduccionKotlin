016071350ec55913a5d098f122c7f050b2b96599
sofiamaring18 <dannasofiamarin@gmail.com>
2024-02-29 22:40:52 -0500

    Update reto_2.kt

--- FundamentosKotlin/app/src/main/java/Intro/reto_2.kt
+++ FundamentosKotlin/app/src/main/java/Intro/reto_2.kt
@@ -1,4 +1,18 @@
 package Intro
 
 class reto_2 {
+    fun sumar() {
+        println("Ingrese los meses del meses del bebe")
+        val mesesbebe = readLine()?.toDoubleOrNull()
+
+        println("Ingrese el peso del bebe")
+        val pesobebe = readLine()?.toDoubleOrNull()
+
+        if (mesesbebe != null && pesobebe != null) {
+            val resultado = ((pesobebe + 10) / (mesesbebe * 10)) * 8
+            println("La dosis del bebe es: $resultado")
+        } else {
+            println("Por favor, ingrese datos válidos.")
+        }
+    }
 }
