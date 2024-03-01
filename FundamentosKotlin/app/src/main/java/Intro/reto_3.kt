62ad804ca1e2699d5d7077becbd8e79068aaf7f7
sofiamaring18 <dannasofiamarin@gmail.com>
2024-02-29 22:56:59 -0500

    Update reto 3

--- FundamentosKotlin/app/src/main/java/Intro/reto_3.kt
+++ FundamentosKotlin/app/src/main/java/Intro/reto_3.kt
@@ -1,4 +1,15 @@
 package Intro
 
 class reto_3 {
+    fun sumar() {
+        println("Ingrese los grados en F°:")
+        val fahrenheit = readLine()?.toDoubleOrNull()
+
+        if (fahrenheit != null) {
+            val resultado = (fahrenheit - 32) / 1.8
+            println("Los grados que ingreso son equivalentes a $resultado grados centigrados")
+        } else {
+            println("Por favor, ingrese un valor válido.")
+        }
+    }
 }
