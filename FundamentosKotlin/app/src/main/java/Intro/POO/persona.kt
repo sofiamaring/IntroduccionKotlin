055dc474629aa0d0108f9a59ce5792746f72e9ff
sofiamaring18 <dannasofiamarin@gmail.com>
2024-03-12 18:16:20 -0500

    Intro POO

---
+++ FundamentosKotlin/app/src/main/java/Intro/POO/persona.kt
@@ -0,0 +1,43 @@
+package Intro.POO
+
+class persona {
+    /*atributos*/
+    private var nombre: String
+        get() {
+            TODO()
+        }
+        set(value) {}
+    var edad: Int
+        get() {
+            TODO()
+        }
+        set(value){}
+    var estatura: Double
+        get() {
+            TODO()
+        }
+        set(value) {}
+    var sexo: Boolean
+        get() {
+            TODO()
+        }
+        set(value) {}
+    constructor()
+    /*constructor (estatura: Double, seco: Boolean, edad: Int, nombre: String) {
+        this.estatura = estatura
+        this.sexo = sexo
+        this.edad = edad
+        this.nombre = nombre
+     } */
+
+    fun pedirDatos(){
+        println()("por favor ingrese su nombre")
+        this.nombre
+        println("Por favor ingrese su edad")
+        this.edad
+    }
+}
+
+    fun main(){
+        val
+    }
